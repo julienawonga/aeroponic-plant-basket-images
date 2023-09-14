b3f4c2170aaa8e68283f34857401159b9e74631a
Julien Awonga <julienawon@gmail.com>
2023-09-14 12:27:46 +0100

    delete folders img

--- scrapper.py
+++ scrapper.py
@@ -9,19 +9,15 @@ from bing_image_downloader import downloader
 import re
 
 """# Scrapping from bing"""
-
-keywords = ["Hydroponic plant baskets", "Aeroponic plant roots", "Plant growth in aeroponics", "Aeroponic plant baskets"]
+keywords = ["red Radish plants", "red Radish greenhouse", "red Radish harvest", "radish Plant growth in aeroponics", "growing hydroponic radishes indoors"]
 
 folders_names = list (map(lambda item : item.title(), keywords))
 folders_names = list (map(lambda item : re.sub(r'\s+', '', item), keywords))
 
-folders_names
+print(folders_names)
 
 for item in folders_names:
   os.mkdir(item, mode=777, dir_fd=None)
 
 for keyword, folder in zip(keywords, folders_names):
   downloader.download(keyword, limit=100,  output_dir=folder, adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
-
-
-
